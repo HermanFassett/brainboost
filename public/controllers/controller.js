52b4b911678a3173bad1a9784fdd4695a750e52c
HermanFassett <hermanfassett@gmail.com>
2015-11-26 13:22:25 -0800

    testing db

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,6 +1,6 @@
 var brainboost = angular.module("brainboost", []);
 brainboost.controller("AppCtrl", ['$scope', '$http', function($scope, $http) {
-  $http.get("/boosts").success(function(response) {
-    $scope.boosts = response;
-  });
+  // $http.get("/boosts").success(function(response) {
+  //   $scope.boosts = response;
+  // });
 }]);
