28614b6e45ddef8faa6a8417368e0f632971bfa7
HermanFassett <hermanfassett@gmail.com>
2015-11-26 12:03:22 -0800

    Adding controllers

---
+++ public/controllers/controller.js
@@ -0,0 +1,4 @@
+var brainboost = angular.module("brainboost", []);
+brainboost.controller("AppCtrl", ['$scope', '$http', function($scope, $http) {
+    console.log("Hello World from controller");
+}]);
