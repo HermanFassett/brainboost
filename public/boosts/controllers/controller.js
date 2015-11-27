a5380ec48ef5615dcaaf18d7281a8bdb1d3e196c
HermanFassett <hermanfassett@gmail.com>
2015-11-27 15:05:34 -0800

    removing test code

--- public/boosts/controllers/controller.js
+++ public/boosts/controllers/controller.js
@@ -1,10 +1,6 @@
 var boost = angular.module("boost", []);
 boost.controller("BoostsCtrl", ['$scope', '$http', function($scope, $http) {
-  $scope.reload = function() {
-    $http.get("/boosts/:boosts").success(function(response) {
-      console.log("/:boosts");
-      $scope.boosts = response;
-    });
-  }
-  $scope.reload();
+  $http.get("/boosts/:boosts").success(function(response) {
+    $scope.boosts = response;
+  });
 }]);
