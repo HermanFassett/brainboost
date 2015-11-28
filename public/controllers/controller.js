fb22b7acd52427414e80e68dd617e83bbe989bd7
HermanFassett <hermanfassett@gmail.com>
2015-11-27 20:58:03 -0800

    Fix

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,6 +1,4 @@
 var brainboost = angular.module("brainboost", []);
 brainboost.controller("AppCtrl", ['$scope', '$http', function($scope, $http) {
-  $http.get("/:all").success(function(response) {
-    $scope.boosts = response;
-  })
+
 }]);
