4e3e18b88e8a12842d3e95b8f8def098c1b3dbdf
HermanFassett <hermanfassett@gmail.com>
2015-12-04 08:38:44 -0800

    Github auth changes

--- app/config/passport.js
+++ app/config/passport.js
@@ -20,28 +20,73 @@ module.exports = function (passport) {
 		clientSecret: configAuth.githubAuth.clientSecret,
 		callbackURL: configAuth.githubAuth.callbackURL
 	},
-	function (token, refreshToken, profile, done) {
-		process.nextTick(function () {
-			User.findOne({ '_id': profile._id }, function (err, user) {
-				if (err) {
-					return done(err);
-				}
-
-				if (user) {
-					return done(null, user);
+	function (req, token, refreshToken, profile, done) {
+		if (req.user) {
+			User.findOne({ github: profile.id }, function(err, existingUser) {
+				if (existingUser) {
+					req.flash('errors', { msg: 'There is already a GitHub account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
+					done(err);
 				} else {
-					var newUser = new User();
-					newUser.id = profile.id;
-					newUser.username = profile.username;
-					newUser.email = profile.email;
-					newUser.save(function (err) {
-						if (err) {
-							throw err;
-						}
-						return done(null, newUser);
+					User.findById(req.user.id, function(err, user) {
+						user.github = profile.id;
+						user.tokens.push({ kind: 'github', accessToken: accessToken });
+						user.profile.name = user.profile.name || profile.displayName;
+						user.profile.picture = user.profile.picture || profile._json.avatar_url;
+						user.profile.location = user.profile.location || profile._json.location;
+						user.profile.website = user.profile.website || profile._json.blog;
+						user.save(function(err) {
+							req.flash('info', { msg: 'GitHub account has been linked.' });
+							done(err, user);
+						});
 					});
 				}
 			});
-		});
+		} else {
+			User.findOne({ github: profile.id }, function(err, existingUser) {
+				if (existingUser) {
+					return done(null, existingUser);
+				}
+				User.findOne({ email: profile._json.email }, function(err, existingEmailUser) {
+					if (existingEmailUser) {
+						req.flash('errors', { msg: 'There is already an account using this email address. Sign in to that account and link it with GitHub manually from Account Settings.' });
+						done(err);
+					} else {
+						var user = new User();
+						user.email = profile._json.email;
+						user.github = profile.id;
+						user.tokens.push({ kind: 'github', accessToken: accessToken });
+						user.profile.name = profile.displayName;
+						user.profile.picture = profile._json.avatar_url;
+						user.profile.location = profile._json.location;
+						user.profile.website = profile._json.blog;
+						user.save(function(err) {
+							done(err, user);
+						});
+					}
+				});
+			});
+		}
+		// process.nextTick(function () {
+		// 	User.findOne({ github: profile.id }, function (err, user) {
+		// 		if (err) {
+		// 			return done(err);
+		// 		}
+		//
+		// 		if (user) {
+		// 			return done(null, user);
+		// 		} else {
+		// 			var newUser = new User();
+		// 			newUser.id = profile.id;
+		// 			newUser.username = profile.username;
+		// 			newUser.email = profile.email;
+		// 			newUser.save(function (err) {
+		// 				if (err) {
+		// 					throw err;
+		// 				}
+		// 				return done(null, newUser);
+		// 			});
+		// 		}
+		// 	});
+		// });
 	}));
 };
