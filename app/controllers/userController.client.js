892829633d15558a711f48ace4f6cccfc536c45d
HermanFassett <hermanfassett@gmail.com>
2015-12-04 15:34:08 -0800

    Testing nav items

--- app/controllers/userController.client.js
+++ app/controllers/userController.client.js
@@ -10,15 +10,15 @@
    var navAuth = $(".auth") || null;
    var navUser = $("#nav-user") || null;
    var apiUrl = appUrl + '/api/:id';
-   navUnauth.each(function(a) { $(a).css("display", "block") });
-   navAuth.each(function(a) { $(a).css("display", "none") });
+   navUnauth.forEach(function(a) { $(navUnauth[a]).css("display", "block") });
+   navAuth.forEach(function(a) { $(navAuth[a]).css("display", "none") });
    ajaxFunctions.ready(ajaxFunctions.ajaxRequest('GET', apiUrl, function (data) {
       var userObject = JSON.parse(data);
       if (navAvatar) $(navAvatar).prop("src", userObject.profile.picture);
       if (userObject.profile.name) {
         $(navUser).html($(navUser).html() + userObject.profile.name);
-        navUnauth.each(function(a) { $(a).css("display", "none") });
-        navAuth.each(function(a) { $(a).css("display", "block") });
+        navUnauth.forEach(function(a) { $(navUnauth[a]).css("display", "none") });
+        navAuth.forEach(function(a) { $(navAuth[a]).css("display", "block") });
       }
       if (profileUsername) $(profileUsername).text(userObject.profile.name);
       if (profileEmail) $(profileEmail).text("Email: " + userObject.email);
