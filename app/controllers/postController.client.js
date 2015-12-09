d62b9bb9d44716d64cdc21749667717cc840fe0f
HermanFassett <hermanfassett@gmail.com>
2015-12-08 16:13:08 -0800

    Fix apiurl

--- app/controllers/postController.client.js
+++ app/controllers/postController.client.js
@@ -16,12 +16,12 @@
 
    addButton.click(function () {
       ajaxFunctions.ajaxRequest('POST', apiUrl + "/up", function () {
-         ajaxFunctions.ajaxRequest('GET', apiUrl, updatePosts);
+         ajaxFunctions.ajaxRequest('GET', apiUrl + "/up", updatePosts);
       });
    });
    deleteButton.click(function () {
       ajaxFunctions.ajaxRequest('POST', apiUrl + "/down", function () {
-         ajaxFunctions.ajaxRequest('GET', apiUrl, updatePosts);
+         ajaxFunctions.ajaxRequest('GET', apiUrl + "/down", updatePosts);
       });
    });
 })();
