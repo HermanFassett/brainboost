7e27e64f5627d144d9fa76f20825def9af377b0b
HermanFassett <hermanfassett@gmail.com>
2015-12-08 16:21:26 -0800

    Jquery function update

--- app/controllers/postController.client.js
+++ app/controllers/postController.client.js
@@ -13,12 +13,12 @@
 
    ajaxFunctions.ready(ajaxFunctions.ajaxRequest('GET', apiUrl, updatePosts));
 
-   addButton.click(function () {
+   $(addButton).click(function () {
       ajaxFunctions.ajaxRequest('POST', apiUrl + "/up", function () {
          ajaxFunctions.ajaxRequest('GET', apiUrl + "/up", updatePosts);
       });
    });
-   deleteButton.click(function () {
+   $(deleteButton).click(function () {
       ajaxFunctions.ajaxRequest('POST', apiUrl + "/down", function () {
          ajaxFunctions.ajaxRequest('GET', apiUrl + "/down", updatePosts);
       });
